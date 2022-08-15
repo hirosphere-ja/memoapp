c6a3364d4f23cfffdfe33a23b730dfe337b2113e
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-08-15 17:25:30 +0900

    format change

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import mysql from 'promise-mysql'// 以下を追加
+import dotenv from "dotenv";
+import express from "express";
+import mysql from "promise-mysql"; // 以下を追加
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 // 以下を追加
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   connection()
     .then((connection) => {
-      const result = connection.query('SELECT * FROM jpmarket_tbl')
-      connection.end()
-      return result
+      const result = connection.query("SELECT * FROM jpmarket_tbl");
+      connection.end();
+      return result;
     })
     .then(function (rows) {
-      res.send(rows)
-    })
-})
+      res.send(rows);
+    });
+});
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
 // 以下を追加
 const db = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-}
+};
 
 const connection = async () => {
-  return await mysql.createConnection(db)
-}
+  return await mysql.createConnection(db);
+};
 
-export default connection
+export default connection;
